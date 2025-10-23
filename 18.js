70f07dde24425465b7865cadbfbced2b7fe8982e
kaifansari14 <khajakaifansari@gmail.com>
2025-10-23 11:43:35 +0000

    coding

--- 18.js
+++ 18.js
@@ -1,1 +1,17 @@
-//
+// Rules For Naming Java Script Variable
+// 1. Variable name be start with either letter undersour or Doller Symble.
+
+let a = "Hello";
+let _b = "hi";
+let $c = "Life";
+console.log(a,_b,$c)
+// 2. Variable name cannot start with Number 
+// let 1a ="Hello", || This gives an error
+/*
+3.Java Script is cax-senitive.So y and Y and different variable 
+*/
+let y ="Hello"
+let Y ="World"
+console.log(y)
+console.log(Y) 
+// 4. Keyword cannot be used as variable name 
---
+++ 19.js
@@ -0,0 +1,16 @@
+let num = Number("123")
+
+console.log(num, typeof num);
+   
+let x 
+console.log(x, typeof x);
+x = 8
+console.log(x, typeof x);
+x = x + ""
+console.log(x, typeof x);
+x = x - 2
+console.log(x, typeof x);
+x = !x
+console.log(x, typeof x);
+
+console.log(Boolean(0))
