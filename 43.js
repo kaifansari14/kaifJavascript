7a14cd957138a95e5d7a04b625d544d10e8e0fe3
kaifansari14 <khajakaifansari@gmail.com>
2025-10-30 15:19:13 +0000

    string Methods & array Methods

---
+++ 43.js
@@ -0,0 +1,81 @@
+// String Methodes
+
+// 1. chartAt()
+
+// let str1 = "Hello";
+// //          01234
+// console.log(str1.charAt());
+
+// 2. concat() method
+
+// let a1 = "Hello";
+// let b1 = " W o r l d";
+
+// console.log(a1.concat(b1));
+
+// 3. includes
+
+// let c1 = "Hello I am CFI Student";
+// console.log(c1.includes("kaif"));
+
+// 4. length
+
+// let d1 = "hello im zohaab love you bitch";
+// console.log(d1.length);
+
+// 5. indexOf 
+
+// let hello = "Hello World";
+// console.log(hello.indexOf("W"));
+
+// 6. lastIndexOf
+
+//let hello1 = "Hey Wassup";
+// console.log(hello1.lastIndexOf("W"));
+
+// 7. repeat() Method 
+
+// let e1 = "Test CFI \n";
+// console.log(e1.repeat(1000));
+
+// // 8. replace()
+
+// let f1 = "Hello World";
+// console.log(f1.replace("World","CFI"));
+
+// 9. split Method
+
+// let g1 = "Hello,I,am,kaif";
+// console.log(g1.split(","));
+
+// 10. twoUpperCase() Method
+
+// let h1 = "hello cfi students";
+// console.log(h1.toLocaleUpperCase());
+
+// 11. toLowerCase() Method
+// let h2 = "hello cfi students";
+// console.log(h2.toLocaleLowerCase());
+
+// 12. trim () method 
+// trim only remove the spaces in side the code back & front
+// let k1 = "     Hello I am CFI    ";
+// console.log(k1.trim());
+
+// 13. slice() Method 
+
+let m1 = "Hello";
+// index  01234
+// lenth  12345
+console.log(m1.slice(0, 4));
+
+
+// 14. PadStart Method()
+
+let n1 = "Hello";
+console.log(n1.padStart(15, "+$"));
+
+// 15. pedEnd () Method 
+
+let n2 = "Heyyy";
+console.log(n2.padEnd(15, "*$"));
